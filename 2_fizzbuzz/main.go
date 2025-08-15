8d26338a5e59e9f72bc86129572495a75861ed87
Michael Knapp <knappmi14@gmail.com>
2025-08-14 23:28:16 -0400

    Solved 2_fizzbuzz

--- 2_fizzbuzz/main.go
+++ 2_fizzbuzz/main.go
@@ -1,39 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 /*
 Problem 2 — FizzBuzz (Loops & Conditionals)
 
 Goal:
-  Print numbers from 1 to 100. Replace multiples of 3 with "Fizz", 5 with "Buzz",
-  and multiples of both with "FizzBuzz".
+
+	Print numbers from 1 to 100. Replace multiples of 3 with "Fizz", 5 with "Buzz",
+	and multiples of both with "FizzBuzz".
 
 Why:
-  Teaches for-loops, if/else, and modulus arithmetic.
+
+	Teaches for-loops, if/else, and modulus arithmetic.
 
 Run:
-  go run ./2_fizzbuzz
+
+	go run ./2_fizzbuzz
 
 Expected (start):
-  1
-  2
-  Fizz
-  4
-  Buzz
-  Fizz
-  7
-  8
-  Fizz
-  Buzz
-  11
-  Fizz
-  13
-  14
-  FizzBuzz
-  ...
+
+	1
+	2
+	Fizz
+	4
+	Buzz
+	Fizz
+	7
+	8
+	Fizz
+	Buzz
+	11
+	Fizz
+	13
+	14
+	FizzBuzz
+	...
 */
 func main() {
-	// TODO: implement
-	fmt.Println("TODO")
+	var str string
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			str = "FizzBuzz"
+		} else if i%3 == 0 {
+			str = "Fizz"
+		} else if i%5 == 0 {
+			str = "Buzz"
+		} else {
+			str = strconv.Itoa(i)
+		}
+		fmt.Println(str)
+	}
 }
